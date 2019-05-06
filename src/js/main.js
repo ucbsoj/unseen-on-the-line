1d2e555f56e3254daefecdb26e252823373befaf
susie <susancneilson@gmail.com>
2019-05-06 15:52:08 -0700

    tried to add chart.js stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,52 @@
 import $ from "jquery"; // importing jQuery, you can delete if not needed
 require("./lib/social"); // Twitter intent JS
-
-
+import Chart from "chart.js";
 
 var imgReveal = $('#top-quote').offset().top;
+
+// creating a chart
+var ctx = document.getElementById('charttwo');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
 
 
 // $("[href*=")
 // $("html,body").stop().animate({scrolTop: })
-
-
 
 $(document).scroll(function() {
 
@@ -34,6 +71,7 @@ $(document).scroll(function() {
 
 
 });
+
 
 
 
