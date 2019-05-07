a52bbcf0e55014930fefed00cb8ec974922515e4
susie <susancneilson@gmail.com>
2019-05-07 14:52:20 -0700

    changed video and did formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,16 @@
 import $ from "jquery"; // importing jQuery, you can delete if not needed
 require("./lib/social"); // Twitter intent JS
-import Chart from "chart.js";
-
-var imgReveal = $('#top-quote').offset().top;
+import Chart from 'chart.js';
 
 // creating a chart
-var ctx = document.getElementById('charttwo');
+var ctx = document.getElementById('chartone');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Nursing and residential care', 'Motor home manufacturing', 'Skiing facilities', 'Recycling facilities (MRFS)', 'Veterinary services', 'Average, all industries'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Injury rate',
+            data: [10.9, 10.3, 10.2, 9.8, 9.8, 3.1,],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -44,33 +42,47 @@ var myChart = new Chart(ctx, {
 });
 
 
+// creating a chart
+var ctx = document.getElementById('charttwo');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['2015', '2016', '2017'],
+        datasets: [{
+            label: 'Injuries per 100 workers',
+            data: [5.1, 6.0, 9.8,],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
 // $("[href*=")
 // $("html,body").stop().animate({scrolTop: })
 
-$(document).scroll(function() {
 
-  var scroll = $(document).scrollTop();
-
-
-  if (scroll > 300) {
-
-  	$(".bg-img").css({"-webkit-filter": "blur(5px)","filter": "blur(5px)" })  
-
-  } else {
-
-  	$(".bg-img").css({"-webkit-filter": "blur(10px)","filter": "blur(10px)" })  
-  
-  }
-
-  if (scroll > imgReveal){
-
-  	$(".bg-img").css({"-webkit-filter": "blur(0px)","filter": "blur(0px)" })  
-  
-  } 
-
-
-});
 
 
 
