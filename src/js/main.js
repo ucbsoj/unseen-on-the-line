f92c261e77fe2f3b08997b881ba19262c01e4d56
susie <susancneilson@gmail.com>
2019-05-08 17:57:39 -0700

    changed a chart

--- src/js/main.js
+++ src/js/main.js
@@ -42,6 +42,43 @@ var myChart = new Chart(ctx, {
 });
 
 
+var ctx = document.getElementById('chartpres');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['2015', '2016', '2017', 'Average for all industries, 2017'],
+        datasets: [{
+            label: 'Injuries per 100 workers',
+            data: [5.1, 6.0, 9.8, 3.1],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
 // creating a chart
 var ctx = document.getElementById('charttwo');
 var myChart = new Chart(ctx, {
@@ -79,8 +116,9 @@ var myChart = new Chart(ctx, {
     }
 });
 
-// $("[href*=")
-// $("html,body").stop().animate({scrolTop: })
+
+
+
 
 
 
