44106c5d0ff13b6d9dcc78521b395d6e6c5d518f
Lucio Villa <lucio310@gmail.com>
2019-05-15 01:24:17 -0700

    Updates

--- app.js
+++ app.js
@@ -29201,29 +29201,42 @@ var myChart = new _chart2.default(ctx, {
 });
 
 // creating a chart
-var ctx = document.getElementById('charttwo');
-var myChart = new _chart2.default(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['2015', '2016', '2017'],
-        datasets: [{
-            label: 'Injuries per 100 workers',
-            data: [5.1, 6.0, 9.8],
-            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+// var ctx = document.getElementById('charttwo');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['2015', '2016', '2017'],
+//         datasets: [{
+//             label: 'Injuries per 100 workers',
+//             data: [5.1, 6.0, 9.8,],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
+
 
 (0, _jquery2.default)(document).ready(function () {
 
